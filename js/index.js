94fd8e21c4a55660441fc214ce867c11600186b4
Daniel <oscar.d.gonzalez@gmail.com>
2024-01-06 14:20:50 -0500

    feat: added toggle price functionality (version w/class & toggle)

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
+// Selecting the toggle button, checkbox, and prices with classes ending in 'annually' or 'monthly'
 const toggle = document.querySelector('.toggle-button');
 const checkbox = toggle.querySelector('input');
 const prices = document.querySelectorAll('[class$="annually"], [class$="monthly"]');
 
+// Function to check if an element's class list contains a specified keyword
+function classHasKeyword(element, keyword) {
+  return new RegExp(keyword).test(element.classList.value);
+}
+
+// Initial loop to hide 'monthly' prices on page load
+prices.forEach(price => {
+  const isMonthly = classHasKeyword(price, 'monthly');
+  if (isMonthly) price.classList.add('hidden');
+});
+
+// Adding event listener for checkbox click
 checkbox.addEventListener('click', function () {
+  // Determining whether to show monthly prices based on checkbox state
   const showMonthly = checkbox.checked;
 
+  // Loop through prices to toggle the 'hidden' class based on checkbox state
   prices.forEach(price => {
-    const isMonthly = /monthly/.test(price.classList.value);
-    price.style.display = isMonthly === showMonthly ? 'flex' : 'none';
+    const isMonthly = classHasKeyword(price, 'monthly');
+    // Toggle 'hidden' class based on whether the price is monthly and checkbox is checked
+    price.classList.toggle('hidden', isMonthly !== showMonthly);
   });
 });
